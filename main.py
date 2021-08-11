9dccdd151909314070629a5a83234f1fbd9b65d3
Ali Al Agamy <55712166+alialagamy@users.noreply.github.com>
2021-08-11 19:25:49 +0200

    created main.py

---
+++ main.py
@@ -0,0 +1,30 @@
+print("   ** ** **** ** **")
+num = 3
+for ctr in range (num):
+    for ctr_2 in range(ctr):
+        print("   **",end="")
+        print ("            **")
+print("   ** ** **** ** **")
+num = 4
+for ctr in range (num):
+    for ctr_2 in range(ctr):
+        print("   **",end="")
+        print ("            **")
+
+num = 4
+for ctr in range (num):
+    for ctr_2 in range(ctr):
+        print("          **")
+print("          ** ** ** **")
+print("\n")
+
+num = 4
+print("    ** ** ** ** **")
+for ctr in range (num):
+    for ctr_2 in range(ctr):
+        print("          **")
+print("    ** ** ** ** **")
+
+
+
+
